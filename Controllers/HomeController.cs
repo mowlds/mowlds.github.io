200abaa0e692ebcf5e047ee87e4312a947bfc7e1
Chris <cpwdb@hotmail.com>
2022-07-04 15:10:26 +0200

    Homepage fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,21 +10,8 @@ namespace mowlds.github.io.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return Redirect("~/index.html");
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
