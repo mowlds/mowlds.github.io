acc931d92c4ab5ebbfe508bc4815b4fc543aeaff
Chris <cpwdb@hotmail.com>
2022-08-17 08:43:17 +0200

    Minor Fixes
    - Added Track List
    - Added Track Page
    - Added NC as option in Season result Supergrid

--- Controllers/TrackController.cs
+++ Controllers/TrackController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using mowlds.github.io.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,31 @@ namespace mowlds.github.io.Controllers
 {
     public class TrackController : Controller
     {
-        // GET: Track
-        public ActionResult Index()
+        private SRLContext1 _context = new SRLContext1();
+
+        public TrackController()
         {
-            return View();
+        }
+
+
+        public TrackController(SRLContext1 context)
+        {
+            _context = context;
+        }
+
+
+        [Route("Track")]
+        public async Task<ActionResult> Index()
+        {
+            var sRLContext = _context.Track;
+            return View(sRLContext);
+        }
+
+        [Route("Track")]
+        public async Task<ActionResult> Track(int track)
+        {
+            var sRLContext = _context.Track.Where(t => t.ID == track);
+            return View(sRLContext.First());
         }
     }
 }
