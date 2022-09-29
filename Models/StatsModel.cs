31ca1a2582fbc3e7cbb5c9691fa9f0c373027844
Chris <cpwdb@hotmail.com>
2022-09-29 18:33:18 +0200

    Stats
    Added stats to track and driver pages

---
+++ Models/StatsModel.cs
@@ -0,0 +1,55 @@
+﻿using mowlds.github.io.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mowlds.github.io.Models
+{
+    public class StatsModel
+    {
+        public string Name { get; set; }
+
+        public int RaceStarts { get; set; }
+
+        public int RaceWins { get; set; }
+
+        public decimal RaceWinPerc { get; set; }
+
+        public int Podiums { get; set; }
+
+        public decimal PodiumPerc { get; set; }
+
+        public int BestFinish { get; set; }
+
+        public int WorstFinish { get; set; }
+
+        public int PointsFinish { get; set; }
+
+        public decimal PointsFinishPerc { get; set; }
+
+        public int DNF { get; set; }
+
+        public decimal DNFPerc { get; set; }
+
+        public int Poles { get; set; }
+
+        public decimal PolePerc { get; set; }
+
+        public int FastestLaps { get; set; }
+
+        public decimal FastestLapsPerc { get; set; }
+
+        public int DriveroftheDay { get; set; }
+
+        public decimal DriveroftheDayPerc { get; set; }
+
+        public int TotalPoints { get; set; }
+
+        public decimal AvgPoints { get; set; }
+
+        public int RaceDSQ { get; set; }
+
+        public int QualyDSQ { get; set; }
+    }
+}
